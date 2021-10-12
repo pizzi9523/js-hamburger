839dc66aeba54bdda0ed5feba1a3fc251d6f3d98
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-12 15:57:44 +0200

    Chiusura menu hamburger on click close

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
 const hamburgerElement = document.querySelector("a>i");
 const menuElement = document.querySelector(".hamburger-menu");
+const closeElement = document.querySelector(".close");
 //console.log(menuElement);
 //console.log(hamburgerElement);
 
 hamburgerElement.addEventListener("click", function () {
     menuElement.classList.add("active")
 })
+
+closeElement.addEventListener("click", function () {
+    menuElement.classList.remove("active")
+})
+
 
 
